460ad1c080e57a61bd950eb58ff96bbb79a571d9
komfyn <yaziddzimarhabibii@gmail.com>
2025-05-27 07:47:54 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,7 +1,10 @@
 import streamlit as st
 
 st.title("KABOOOOOOOMMM")
-st.write(
-    "her pistol go (bang bang boom boom pop pop)"
-)
-st.image("bb91fffeef307c6a3a09c46f63341d11.jpg")
+st.write("Mengecek nilai genap/ganjil")
+num = st.number_input("Tulis sebuah angka:", value=0, step=1)
+
+if (num % 2) == 0:
+   st.write(f"{num} adalah bilangan genap")
+else:
+   st.write(f"{num} adalah bilangan ganjil")
