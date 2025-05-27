ba27c2688ed35725698fdf6221953c415039740f
komfyn <yaziddzimarhabibii@gmail.com>
2025-05-27 07:59:21 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,7 @@
 import streamlit as st
 
 st.title("KABOOOOOOOMMM")
+st.image("bb91fffeef307c6a3a09c46f63341d11.jpg")
 st.write("Mengecek nilai genap/ganjil")
 num = st.number_input("Tulis sebuah angka:", value=0, step=1)
 
